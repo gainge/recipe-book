2f4499eafa3e006465143ecb4fe2c692f5ee0dc9
Gibson Ainge <gainge@gmail.com>
2019-07-21 16:07:40 -0600

    Added connection manager for api

---
+++ api/config/database.php
@@ -0,0 +1,22 @@
+<?php
+class Database {
+
+	// Define some cool variables my guy
+	private $db_name = "api_db";
+	public $conn;
+
+	// Get the database connection
+	public function getConnection() {
+		$this->conn = null;
+
+		try {
+			$this->conn = new PDO("sqlite:../../" . $this->db_name . ".db");
+		} catch (PDOException $exception) {
+			echo "Connection error: " . $exception->getMessage();
+		}
+
+		// Give the database back, or whatever
+		return $this->conn;
+	}
+}
+?>
